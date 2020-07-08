460975002674359c08d3eb4fc0f874d725c74ff0
Joseph Ryan <josephryan3.14@gmail.com>
2020-07-08 08:41:05 -0500

    Start adding copies of `Clean` types with Serde derives

--- src/librustdoc/json/mod.rs
+++ src/librustdoc/json/mod.rs
@@ -1,5 +1,9 @@
+mod conversions;
 mod types;
 
+// use std::fs::File;
+
+use rustc_data_structures::fx::FxHashMap;
 use rustc_span::edition::Edition;
 
 use crate::clean;
@@ -9,7 +13,9 @@ use crate::formats::cache::Cache;
 use crate::formats::FormatRenderer;
 
 #[derive(Clone)]
-pub struct JsonRenderer {}
+pub struct JsonRenderer {
+    index: FxHashMap<types::DefId, types::Item>,
+}
 
 impl FormatRenderer for JsonRenderer {
     fn init(
@@ -20,21 +26,30 @@ impl FormatRenderer for JsonRenderer {
         _cache: &mut Cache,
     ) -> Result<(Self, clean::Crate), Error> {
         debug!("Initializing json renderer");
-        Ok((JsonRenderer {}, krate))
+        Ok((JsonRenderer { index: FxHashMap::default() }, krate))
     }
 
     fn item(&mut self, item: clean::Item, _cache: &Cache) -> Result<(), Error> {
-        debug!("Documenting item: {:?}", item.name.unwrap_or_default());
+        debug!(
+            "Documenting item: {}: {}",
+            item.clone().name.unwrap_or_default().as_str(),
+            serde_json::to_string(&types::Item::from(item)).unwrap().as_str()
+        );
+        // self.index.insert(item.def_id.into(), item.into()).unwrap();
         Ok(())
     }
 
     fn mod_item_in(
         &mut self,
-        _item: &clean::Item,
+        item: &clean::Item,
         item_name: &str,
         _cache: &Cache,
     ) -> Result<(), Error> {
-        debug!("Entering module: {}", item_name);
+        debug!(
+            "Entering module: {}: {}",
+            item_name,
+            serde_json::to_string(&types::Item::from(item.clone())).unwrap().as_str(),
+        );
         Ok(())
     }
 
@@ -45,6 +60,19 @@ impl FormatRenderer for JsonRenderer {
 
     fn after_krate(&mut self, _krate: &clean::Crate, _cache: &Cache) -> Result<(), Error> {
         debug!("Done with crate");
+        // debug!("Index: {:?}", self.index);
+        // debug!("Impls: {:?}", cache.impls);
+        // debug!("Implementors: {:?}", cache.implementors);
+        // debug!("Traits: {:?}", cache.traits);
+        // debug!("Paths: {:?}", cache.paths);
+        // for item in self.index.values() {
+        //     debug!("{}", serde_json::to_string(item).unwrap());
+        // }
+        // serde_json::to_writer(
+        //     &File::create("test.json").unwrap(),
+        //     &types::Item::from(krate.module.clone().unwrap()),
+        // )
+        // .unwrap();
         Ok(())
     }
 
